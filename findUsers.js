85d30b1036c0a562f35ffdcf869ad98ee3878b20
Oleksandr Dzebovski <104393943+dzebski@users.noreply.github.com>
2022-07-20 16:15:25 +0300

    Create findUsers.js

---
+++ findUsers.js
@@ -0,0 +1,24 @@
+// Є масив обєктів age і name, треба шоб на виході був масив стрінгів імен в аперкейсі 
+// з обєктів де age більше 15.
+// [{ age: 10, name: 'Test' }, { age: 20, name: 'Test' }].transformUpper()
+
+const sampleData = [
+    {
+        age: 10,
+        name: 'Test1'
+    },
+    {
+        age: 20,
+        name: 'Test2'
+    }
+]
+
+const usersWithAge = () => {
+    let result = []
+    for (const user of sampleData)
+        if (user.age > 15)
+            result.push(user.name.toUpperCase())
+    return result
+}
+
+console.table(usersWithAge())
